e24dc3cb291f6717643021ccb898c3f5a89b5d8f
Debug <49997488+DebugOk@users.noreply.github.com>
2023-06-13 20:00:42 +0200

    Remove leftover underclothing code
    May or may not be added to Delta

--- Content.Shared/Roles/StartingGearPrototype.cs
+++ Content.Shared/Roles/StartingGearPrototype.cs
@@ -43,39 +43,6 @@ namespace Content.Shared.Roles
                     return _satchel;
                 if (slot == "back" && profile.Backpack == BackpackPreference.Duffelbag && !string.IsNullOrEmpty(_duffelbag))
                     return _duffelbag;
-
-                // Handles equipping all crew with underwear without putting it in every file.
-                // Checks for skirt settings, if skirt = true, equip with panties and bra
-                // if skirt = false, equip with boxers and shirt.
-                // Using else caused weird issues I didn't feel like dealing with -Psp
-
-                if (slot == "underpants" && profile.Clothing != ClothingPreference.Jumpskirt && string.IsNullOrEmpty(_underpants))
-                    return "ClothingUnderboxer_briefs";
-                if (slot == "underpants" && profile.Clothing == ClothingPreference.Jumpskirt && string.IsNullOrEmpty(_underpantsskirt))
-                    return "ClothingUnderpanties";
-
-                if (slot == "undershirt" && profile.Clothing != ClothingPreference.Jumpskirt && string.IsNullOrEmpty(_undershirt))
-                    return "ClothingUnderundershirt";
-                if (slot == "undershirt" && profile.Clothing == ClothingPreference.Jumpskirt && string.IsNullOrEmpty(_undershirtskirt))
-                    return "ClothingUnderbra";
-
-                if (slot == "socks" && string.IsNullOrEmpty(_undersocks))
-                    return "ClothingUnderSocks_norm";
-
-                // Handles custom underwear per role.
-
-                if (slot == "underpants" && profile.Clothing != ClothingPreference.Jumpskirt && !string.IsNullOrEmpty(_underpants) && _underpants != "empty")
-                    return _underpants;
-                if (slot == "underpants" && profile.Clothing == ClothingPreference.Jumpskirt && !string.IsNullOrEmpty(_underpantsskirt) && _underpantsskirt != "empty")
-                    return _underpantsskirt;
-
-                if (slot == "undershirt" && profile.Clothing != ClothingPreference.Jumpskirt && !string.IsNullOrEmpty(_undershirt) && _undershirt != "empty")
-                    return _undershirt;
-                if (slot == "undershirt" && profile.Clothing == ClothingPreference.Jumpskirt && !string.IsNullOrEmpty(_undershirtskirt) && _undershirtskirt != "empty")
-                    return _undershirtskirt;
-
-                if (slot == "socks" && !string.IsNullOrEmpty(_undersocks) && _undersocks != "empty")
-                    return _undersocks;
             }
 
             return _equipment.TryGetValue(slot, out var equipment) ? equipment : string.Empty;
